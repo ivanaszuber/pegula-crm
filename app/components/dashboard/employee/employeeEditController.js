0742b224a63057bf7ae7ffc5a99cd8e135b1bb59
Ivana Zuber <ivanaz@counterparty.io>
2015-07-19 12:43:12 +0200

    added fields in editEmployee view

--- app/components/dashboard/employee/employeeEditController.js
+++ app/components/dashboard/employee/employeeEditController.js
@@ -1,36 +1,87 @@
 /**
  * Created by Ivana on 1.6.2015..
  */
-define(['appModule'], function (module) {
+define(['appModule', 'moment'], function (module, moment) {
 
     'use strict';
 
-    module.registerController('employeeEditController', function ($scope, $state, employeeService, $rootScope) {
+    module.registerController('employeeEditController', function ($scope, $state, employeeService, $stateParams) {
 
         $scope.employee = {};
+
+        $scope.employeeRoles = [
+            'Frontend Developer',
+            'Backend Developer',
+            'Database Administrator',
+            'President',
+            'CEO',
+            'Managing Director',
+            'Project Manager'
+        ];
+
+        $scope.employeeStatus = [
+            'Full Time',
+            'Contract',
+            'Candidate'
+        ];
+
+
+        $scope.submitted = false;
+
+        /**
+         * Open the DateFrom datepicker
+         * @param $event
+         */
+        $scope.openDateFrom = function ($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+            if (typeof($scope.mydp) === 'undefined') {
+                $scope.mydp = {};
+            }
+            $scope.mydp.openedDateFrom = true;
+        };
+
+        /**
+         * Open the DateTo datepicker
+         * @param $event
+         */
+        $scope.openDateTo = function ($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+            if (typeof($scope.mydp) === 'undefined') {
+                $scope.mydp = {};
+            }
+            $scope.mydp.openedDateTo = true;
+        };
 
         /**
          * Get selected employee data
          */
-        employeeService.getEmployee($rootScope.selectedEmployee).then(function (data) {
+        employeeService.getEmployee($stateParams.employee_id).then(function (data) {
             $scope.employee = data;
-        })
+        });
 
 
         /**
          * Update the selected employee data, go to the All state and
          * display a success notification
          */
-        $scope.update = function () {
-            employeeService.updateEmployee($rootScope.selectedEmployee, $scope.employee).then(function () {
-                $state.go('app.employees', {employeesType:'All'});
-                $.smallBox({
-                    title: "Employee "+ $scope.employee.first_name + ' ' + $scope.employee.last_name + ' has been modified.',
-                    color: "#739E73",
-                    iconSmall: "fa fa-check",
-                    timeout: 5000
+        $scope.update = function (isValid) {
+
+            $scope.submitted = true;
+
+            if(isValid){
+                employeeService.updateEmployee($stateParams.employee_id, $scope.employee).then(function () {
+                    $state.go('app.employees', {employeesType:'All'});
+                    $.smallBox({
+                        title: "Employee "+ $scope.employee.first_name + ' ' + $scope.employee.last_name + ' has been modified.',
+                        color: "#739E73",
+                        iconSmall: "fa fa-check",
+                        timeout: 5000
+                    });
                 });
-            });
+            }
+
         }
     })
 })
